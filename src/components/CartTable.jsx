9771cda34f98e11edf074b6cb8daae9432c068ee
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-24 21:02:20 +0200

    total price calculated from context

--- src/components/CartTable.jsx
+++ src/components/CartTable.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import CartTableRow from "./CartTableRow";
 import { arrayOfProducts as products } from "../data.json";
 import classes from "./css/CartTable.module.css";
+import { arrayOfProducts as products } from "../data.json";
 
-const CartTable = () => {
+const CartTable = ({ items }) => {
+  const totalPrice = items.reduce((total, item) => {
+    return (
+      total + item.quantity * products.find(p => p.name === item.name).price
+    );
+  }, 0);
+
   return (
     <table style={{ width: "100%", borderSpacing: 0 }}>
       <tbody>
-        {products.map((product, idx) => (
-          <CartTableRow key={idx} product={product} rowNumber={idx + 1} />
+        {items.map((item, idx) => (
+          <CartTableRow key={idx} item={item} rowNumber={idx + 1} />
         ))}
         <tr className={classes.totalRow}>
           <td></td>
