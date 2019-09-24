4c6c7e354b375e3ecbd323be17f50e8295e30c7e
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-24 21:06:41 +0200

    conditionally render table and checkout btn

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartTable from "../components/CartTable.jsx";
 import classes from "./css/Cart.module.css";
+import { CartContext } from "../context/CartContext.jsx";
 
 const Cart = () => {
+  const { items } = useContext(CartContext);
   return (
     <div className={classes.root}>
       <h1 className={classes.title}>Your cart</h1>
-      <CartTable />
+      {items.length === 0 ? (
+        <div>You don't have any products in your cart.</div>
+      ) : (
+        <>
+          <CartTable items={items} />
+          <div className={classes.checkoutContainer}>
+            <button className="button">Checkout</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
