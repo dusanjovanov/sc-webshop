466dd3b3edb024d02025b9fc4a7ede6afc0994b8
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-24 18:33:46 +0200

    cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import CartTable from "../components/CartTable.jsx";
+import classes from "./css/Cart.module.css";
 
 const Cart = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className={classes.root}>
+      <h1 className={classes.title}>Your cart</h1>
+      <CartTable />
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
