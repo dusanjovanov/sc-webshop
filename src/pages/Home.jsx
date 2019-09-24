843d074546c6759bddf8dd457dedbde470e00211
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-24 15:01:37 +0200

    added product list to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import classes from "./css/Home.module.css";
+import ProductList from "../components/ProductList";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className={classes.root}>
+      <ProductList />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
