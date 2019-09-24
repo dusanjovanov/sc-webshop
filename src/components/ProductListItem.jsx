40ecd421c9194cceb24ca791513cd4c4eb78e2ff
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-24 15:00:59 +0200

    product list item component

---
+++ src/components/ProductListItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./css/ProductListItem.module.css";
+import { Link } from "react-router-dom";
+
+const ProductListItem = ({ product }) => {
+  return (
+    <Link
+      to={`/product/${product.name.replace(/\s/g, "-")}`}
+      className={classes.root}
+    >
+      <div>
+        <img
+          className={classes.image}
+          src={product.imgUrl}
+          alt={`image of ${product.name}`}
+        />
+      </div>
+      <div className={classes.title}>
+        <div className={classes.name}>{product.name}</div>{" "}
+        <div className={classes.price}>€{product.price}</div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductListItem;
