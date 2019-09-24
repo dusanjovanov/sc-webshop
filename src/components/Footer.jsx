9045d7ad0ba75db0cb029d09cbe330d6c1b135d0
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-24 21:05:37 +0200

    added a footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import classes from "./css/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={classes.root}>
+      <div className={classes.brand}>Some shop</div>
+      <ul>
+        <li>Lorem ipsum</li>
+        <li>dolor sit amet</li>
+        <li>consectetur adipiscing</li>
+        <li>elit Pellentesque</li>
+        <li>faucibus</li>
+      </ul>
+      <ul>
+        <li>Lorem ipsum</li>
+        <li>dolor sit amet</li>
+        <li>consectetur adipiscing</li>
+        <li>elit Pellentesque</li>
+        <li>faucibus</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
+
+// . , elit Pellentesque faucibus purus a leo vulputate
+//         viverra. Quisque vehicula nisl sollicitudin, mattis sem vitae, finibus
+//         metus. Curabitur eleifend dui et tellus tempus, venenatis tincidunt eros
+//         pellentesque.
