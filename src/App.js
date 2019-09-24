8887daa3bb68373f2600395ff894edc04887145e
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-24 13:52:35 +0200

    added empty pages and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/product/:name" component={Product} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
