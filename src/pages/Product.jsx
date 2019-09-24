c978cebdf13c1d1a392790afcc6a67c60353b421
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-24 15:46:40 +0200

    product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { arrayOfProducts as products } from "../data.json";
+import classes from "./css/Product.module.css";
 
-const Product = () => {
-    return (
-        <div>
-            
+const Product = ({ match }) => {
+  const productName = match.params.name.replace(/-/g, " ");
+  const product = products.find(p => p.name === productName);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.productRoot}>
+        <div className={classes.imageContainer}>
+          <img
+            className={classes.image}
+            src={product.imgUrl}
+            alt={product.name}
+          />
         </div>
-    )
-}
+        <div className={classes.right}>
+          <h1 className={classes.name}>{product.name}</h1>
+          <div className={classes.description}>{product.description}</div>
+          <div className={classes.priceAddToCart}>
+            <button className={classes.addToCartButton}><span role="img" aria-label="cart">🛒</span> Add to cart</button>
+            <div className={classes.price}>€{product.price}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
